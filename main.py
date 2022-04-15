b4f653aa7ceeaa17649946e232c47088f20e5584
rbtroselli <rbt.roselli@gmail.com>
2022-04-16 01:15:50 +0200

    File open and link format

--- main.py
+++ main.py
@@ -11,27 +11,42 @@ i = 0
 dict = {'us':'', 'it':'it.', 'uk':'uk.'}
 
 # open file to write results
-with open('output.csv','w') as f:
+# avoid with for indentation
+f = open('output.csv','w')
+o = open('output.txt','w')
 
-    # iterate for every country in dictionary
-    for key in dict:
-        url = f'https://{dict[key]}indeed.com/jobs?q="data%20engineer"&start='
-        
-        j=1
+# iterate for every country in dictionary
+for key in dict:
+    url = f'https://{dict[key]}indeed.com/jobs?q="data%20engineer"&start='
+    
+    # just checking number of posts per page in CSV
+    j=1
 
-        # iterate for every page (1-66)
-        for i in range(0,2):
-            url2 = url + str(i*10)
-            print(url2)
+    # iterate for every page (1-66)
+    for i in range(0,3):
+        url2 = url + str(i*10)
+        print(url2)
 
-            # get the page, parse with beautifulsoup
-            page = requests.get(url2)
-            soup = BeautifulSoup(page.content,'html.parser')   
+        # get the page, parse with beautifulsoup
+        page = requests.get(url2)
+        soup = BeautifulSoup(page.content,'html.parser') 
 
-            # iterate for every post in page
-            # find every a tag with 'result' as part of the class name
-            for element in soup.find_all('a', {'class':re.compile(r'result')}):
-                # write the country dict key, the url, the job id
-                f.write(f"{element['id']},https://{dict[key]}indeed.com{element['href']},{key},{j}\n")
-                j+=1
-            time.sleep(3)
+        #printing response
+        print(page)
+
+        # check why pages jump sometimes
+        o.write(soup.text)
+        o.write('\n\n\n\n\n\n\n\n\n\n\n\n######################\n\n\n\n\n\n\n\n\n\n\n')  
+
+        # iterate for every post in page
+        # find every a tag with 'result' as part of the class name
+        for element in soup.find_all('a', {'class':re.compile(r'result')}):
+            print(j)
+            time.sleep(0.5)
+            # write the country dict key, the url, the job id
+            f.write(f"{element['id']},https://{dict[key]}indeed.com/viewjob{element['href'][7:]},{key},{j}\n")
+            j+=1
+        time.sleep(5)
+
+f.close()
+o.close()
