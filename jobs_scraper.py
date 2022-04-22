9f5e39e844fd60f21013d91065b2fd62824a922d
rbtroselli <rbt.roselli@gmail.com>
2022-04-22 17:22:00 +0200

    Fixed site page loop at last page

--- jobs_scraper.py
+++ jobs_scraper.py
@@ -16,50 +16,55 @@ def url_scraper(path=''):
     print(dict)
     dict = {'it':'it.'}
     urls_file = path + 'data/urls.csv'
-
-    # open file to write results
-    # avoid with for indentation
     f = open(urls_file,'w')
-    f.write("job_id,country,number,job_url\n")
+    f.write("number,job_id,country,job_url\n")
 
     # iterate for every country in dictionary
     for key in dict:
         url = f'https://{dict[key]}indeed.com/jobs?q="data+engineer"&sort=date&fromage=1&filter=0&start='
+        # only last day &fromage=1
         
         # just checking number of posts per page in CSV
         j=1
-        # break condition to avoid looping at last page
-        id = ''
+
+        # keep same sessione
+        s = requests.Session()
+        # df_list = []
+        id_list = []
 
         # iterate for every page (1-66)
         for i in range(0,1000):
-            # if captcha then change proxy
-
-
-            header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" ,
-                        'referer':'https://www.google.com/' }
-
+            # idea: if captcha then change proxy?!
             # combine the url, get the page, parse with bs
             url2 = url + str(i*10)
-            page = requests.get(url2, headers=header)
+            page = s.get(url2)
             soup = BeautifulSoup(page.content,'html.parser')
-            
-            print (id, url2)
-            if id in soup.find_all('a', {'id':re.compile(r'job_')}): break
+
+            # break condition, if url is already present Indeed page is looping
+            if soup.find('a', {'id':re.compile(r'job_')})['id'][4:] in id_list: break
 
             # iterate for every post in page
             # find every a tag with 'job_' as part of the id name
             for element in soup.find_all('a', {'id':re.compile(r'job_')}):
-                print(j)
                 time.sleep(0.2)
                 # take job id
                 id = element['id'][4:]
-                # job id is sufficient to retrieve the job page
-                # write if, country, n, link
-                f.write(f"{id},{key},{j},https://www.indeed.com/viewjob?jk={id}\n")
+                f.write(f"{j},{id},{key},https://www.indeed.com/viewjob?jk={id}\n")
+                # row = {
+                #     'job_id':id,
+                #     'country':key,
+                #     'number':j,
+                #     'job_url':f'https://www.indeed.com/viewjob?jk={id}'
+                # }
+                # df_list.append(row)
+                id_list.append(id)
+                print(j,id)
                 j+=1
-            
             time.sleep(5)
+    
+    # df = pd.DataFrame(df_list)
+    # print(df)
+    # df.to_csv(urls_file, index=False)
     f.close()
     return
 
