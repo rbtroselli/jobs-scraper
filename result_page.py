430e106bd2037b4e05a96067819b38c5dd8ea386
rbtroselli <rbt.roselli@gmail.com>
2023-07-02 04:51:30 +0200

    rename to result_page

--- result_page.py
+++ result_page.py
@@ -1,26 +1,22 @@
-# classe, ha un attributo che è la lista degli url, che ritorna scrapati al main, che li checka contro un db e li aggiunge se mancano
-
-class SearchPage:
-    def __init__(self, url, driver):
+class ResultPage:
+    """ A class to represent a result page, with post urls """
+    def __init__(self, url, driver, keyword):
         self.url = url
         self.driver = driver
         self.posts = []
-        self.search_term = ''
-
-
-        self.scrape()
+        self.keyword = keyword.replace('+',' ')
+        self._scrape()
         return
 
-    def scrape(self):
+    def _scrape(self):
         self.driver.get(self.url)
-
-
         posts = self.driver.find_elements('class name', 'jcs-JobTitle.css-jspxzf.eu4oa1w0')
         for post in posts:
             id = post.get_attribute('id').replace('job_','')
             self.posts.append({
                 'id': id,
-                'url': f'https://www.indeed.com/viewjob?jk={id}'
+                'url': f'https://www.indeed.com/viewjob?jk={id}',
+                'keyword': self.keyword
             })
         return
     
@@ -28,4 +24,8 @@ class SearchPage:
         for post in self.posts:
             print(post['id'])
             print(post['url'])
+            print(post['keyword'])
         return
+
+    def get_posts(self):
+        return self.posts
