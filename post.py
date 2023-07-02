119a6f7c1dd086908e923a84286ab776c6326f3e
rbtroselli <rbt.roselli@gmail.com>
2023-07-03 00:20:29 +0200

    add attributes, add get dict function

--- post.py
+++ post.py
@@ -4,8 +4,10 @@ from datetime import datetime
 
 class Post:
     """ A class to represent a single job post """
-    def __init__(self, url, driver):
+    def __init__(self, url, id, search_terms, driver):
         self.url = url
+        self.id = id
+        self.search_terms = search_terms
         self.driver = driver
         self.scrape_timestamp = datetime.now()
         self.title = None
@@ -13,7 +15,6 @@ class Post:
         self.company = None
         self.posted_date = None
         # location
-        self.raw_job_location = None
         self.address_country = None
         self.address_locality = None
         self.address_region_0 = None
@@ -21,13 +22,10 @@ class Post:
         self.address_region_2 = None
         self.postal_code = None
         # hiring organization
-        self.raw_hiring_organization = None
         self.hiring_organization = None
         # location requirements
-        self.raw_location_requirements = None
         self.country_requirements = None
         # salary
-        self.raw_base_salary = None
         self.min_salary = None
         self.max_salary = None
         self.salary_currency = None
@@ -54,7 +52,6 @@ class Post:
         script_dict = json.loads(script_json)
         self.posted_date = script_dict.get('datePosted','').split('T')[0]
         # location
-        self.raw_job_location = script_dict.get('jobLocation','')
         self.address_country = script_dict.get('jobLocation','').get('address','').get('addressCountry','')
         self.address_locality = script_dict.get('jobLocation','').get('address','').get('addressLocality','')
         self.address_region_0 = script_dict.get('jobLocation','').get('address','').get('addressRegion','')
@@ -62,17 +59,14 @@ class Post:
         self.address_region_2 = script_dict.get('jobLocation','').get('address','').get('addressRegion2','')
         self.postal_code = script_dict.get('jobLocation','').get('address','').get('postalCode','')
         # hiring organization
-        self.raw_hiring_organization = script_dict.get('hiringOrganization','')
         self.hiring_organization = script_dict.get('hiringOrganization','').get('name','')
         # location requirements
         try:
-            self.raw_location_requirements = script_dict.get('applicantLocationRequirements','')
             self.country_requirements = script_dict.get('applicantLocationRequirements','').get('name','')
         except:
             pass
         # salary
         try:
-            self.raw_base_salary = script_dict.get('baseSalary','')
             self.salary_currency = script_dict.get('baseSalary','').get('currency','')
             self.min_salary = script_dict.get('baseSalary','').get('value','').get('minValue','')
             self.max_salary = script_dict.get('baseSalary','').get('value','').get('maxValue','')
@@ -91,13 +85,13 @@ class Post:
     def display(self):
         print('-----------------')
         print(f'URL: {self.url}')
+        print(f'ID: {self.id}')
+        print(f'Search Terms: {self.search_terms}')
         print(f'Scrape Timestamp: {self.scrape_timestamp}')
         print(f'Title: {self.title}')
-        print(f'Content: {self.content[:100]}...')
         print(f'Company: {self.company}')
         print(f'Posted Date: {self.posted_date}')
         # location
-        print(f'Raw Job Location: {self.raw_job_location}')
         print(f'Address Country: {self.address_country}')
         print(f'Address Locality: {self.address_locality}')
         print(f'Address Region 0: {self.address_region_0}')
@@ -105,13 +99,10 @@ class Post:
         print(f'Address Region 2: {self.address_region_2}')
         print(f'Postal Code: {self.postal_code}')
         # hiring organization
-        print(f'Raw Hiring Organization: {self.raw_hiring_organization}')
         print(f'Hiring Organization: {self.hiring_organization}')
         # location requirements
-        print(f'Raw Location Requirements: {self.raw_location_requirements}')
         print(f'Country Requirements: {self.country_requirements}')
         # salary
-        print(f'Raw Base Salary: {self.raw_base_salary}')
         print(f'Salary Currency: {self.salary_currency}')
         print(f'Min Salary: {self.min_salary}')
         print(f'Max Salary: {self.max_salary}')
@@ -122,6 +113,42 @@ class Post:
         print(f'Valid Through Date: {self.valid_through_date}')
         print(f'Direct Apply: {self.direct_apply}')
         print(f'Title Again: {self.title_again}')
+        print(f'Content: {self.content[:100]}...')
         print(f'Raw Script Json: {self.raw_script_json[:100]}...')
         print('-----------------')
         return
+    
+    def get_post_dict(self):
+        return {
+            'url': self.url,
+            'id': self.id,
+            'search_terms': self.search_terms,
+            'scrape_timestamp': self.scrape_timestamp,
+            'title': self.title,
+            'company': self.company,
+            'posted_date': self.posted_date,
+            # location
+            'address_country': self.address_country,
+            'address_locality': self.address_locality,
+            'address_region_0': self.address_region_0,
+            'address_region_1': self.address_region_1,
+            'address_region_2': self.address_region_2,
+            'postal_code': self.postal_code,
+            # hiring organization
+            'hiring_organization': self.hiring_organization,
+            # location requirements
+            'country_requirements': self.country_requirements,
+            # salary
+            'salary_currency': self.salary_currency,
+            'min_salary': self.min_salary,
+            'max_salary': self.max_salary,
+            'salary_unit': self.salary_unit,
+            # other stuff
+            'job_location_type': self.job_location_type,
+            'employment_type': self.employment_type,
+            'valid_through_date': self.valid_through_date,
+            'direct_apply': self.direct_apply,
+            'title_again': self.title_again,
+            'content': self.content,
+            'raw_script_json': self.raw_script_json
+        }
