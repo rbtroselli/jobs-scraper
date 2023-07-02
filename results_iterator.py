3d8be6e2fe8d4d96ad5773804066da0ed1baaef2
rbtroselli <rbt.roselli@gmail.com>
2023-07-02 14:27:06 +0200

    url iteration, stop condition, save to file

--- results_iterator.py
+++ results_iterator.py
@@ -11,14 +11,16 @@ class ResultsIterator:
 
     def _iterate_pages(self, keyword):
         """ Iterate through the pages of a SINGLE KEYWORDS search results """
-        for i in range(0, 20, 10):
-            url = f'https://www.indeed.com/jobs?q=%22{keyword}%22&sort=date&filter=0&start={i}'
+        for i in range(0, 10000, 10):
+            url = f'https://www.indeed.com/jobs?q=%22{keyword}%22&sort=date&start={i}'
             result_page = ResultPage(url, self.driver, keyword)
             result_page.display()
             posts = result_page.get_posts()
+            if all(post['id'] in [e['id'] for e in self.posts_list] for post in posts):
+                break # if all posts ids are already in posts_list, break
             for post in posts:
                 self.posts_list.append(post)
-            time.sleep(random.uniform(5,10))
+            time.sleep(random.uniform(5,15))
         return
 
     def _iterate_keywords(self):
@@ -31,3 +33,10 @@ class ResultsIterator:
         """ Return the list of posts """
         self._iterate_keywords()
         return self.posts_list
+    
+    def save_posts(self):
+        with open('posts.csv','w') as f:
+            f.write('id,url,keyword,scrape_date\n')
+            for post in self.posts_list:
+                f.write(f"{post['id']},{post['url']},{post['keyword']},{post['scrape_date']}\n")
+        return
