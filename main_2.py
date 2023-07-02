c8f023d72dd9d1da125c345b6c1f8cb9540a16e0
rbtroselli <rbt.roselli@gmail.com>
2023-07-03 00:21:54 +0200

    Update main_2.py

--- main_2.py
+++ main_2.py
@@ -1,18 +1,7 @@
-from functions import get_driver, get_search_terms_list
+# get all urls in a csv file, in a later step check if they are already in the db, and add to the db (with id, url, search term, insert date)
 from results_iterator import ResultsIterator
 
-# get all urls in a csv file, in a later step check if they are already in the db, and add to the db (with id, url, search term, insert date)
-
-
-
-
-
 if __name__ == '__main__':
-    search_terms_list = get_search_terms_list()
-    driver = get_driver()
-
-    results_iterator = ResultsIterator(search_terms_list, driver)
+    results_iterator = ResultsIterator()
     results_iterator.scrape_results()
     results_iterator.save_results()
-
-    driver.quit()
