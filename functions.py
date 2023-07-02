29f195e5b9ba76c1fce9c249b371aa87b2543efd
rbtroselli <rbt.roselli@gmail.com>
2023-07-02 21:37:50 +0200

    remove driver, with new selenium

--- functions.py
+++ functions.py
@@ -1,14 +1,14 @@
+# Browser driver not needed anymore, since we are using selenium-manager
+# https://www.selenium.dev/blog/2022/introducing-selenium-manager/
 from selenium import webdriver
 
 def get_driver():
     """ Return a driver to use selenium """
-    chrome_driver_path='./browser/chromedriver'
     user_data_dir = './browser/user_data' # local data folder
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument(f'--user-data-dir={user_data_dir}')
-    # chrome_options.binary_location = './browser/chrome.app' # use standalone chrome app
 
-    driver = webdriver.Chrome(executable_path=chrome_driver_path, options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(10) # ?
     return driver
 
