669459c290fff7cb7e47f7656247adcc95f5395c
rbtroselli <rbt.roselli@gmail.com>
2023-07-03 00:21:26 +0200

    driver and search terms inside class

--- results_iterator.py
+++ results_iterator.py
@@ -1,13 +1,15 @@
 import time
 import random
 from result_page import ResultPage
+from functions import get_driver, get_search_terms_list
 
 class ResultsIterator:
-    """ A class to iterate through the search results, save and eventually return a list of results urls """
-    def __init__(self, search_terms_list, driver):
-        self.search_terms_list = search_terms_list
-        self.driver = driver
+    """ A class to iterate through the search results, combine all the pages and save them """
+    def __init__(self):
+        self.search_terms_list = get_search_terms_list()
+        self.driver = get_driver()
         self.results_list = []
+        return
 
     def _iterate_pages(self, search_terms):
         """ Iterate through the pages of a SINGLE SEARCH TERMS combo results """
@@ -15,7 +17,7 @@ class ResultsIterator:
             url = f'https://www.indeed.com/jobs?q=%22{search_terms}%22&sort=date&start={i}'
             result_page = ResultPage(url, self.driver, search_terms)
             result_page.display()
-            local_results = result_page.get_results_list()
+            local_results = result_page.get_results_dict_list()
             if all(result['id'] in [e['id'] for e in self.results_list] for result in local_results):
                 break # if all results ids are already in results_list, break
             for result in local_results:
@@ -27,6 +29,7 @@ class ResultsIterator:
         """ Iterate through the search terms list """
         for search_terms in self.search_terms_list:
             self._iterate_pages(search_terms)
+        self.driver.quit()
         return
 
     def scrape_results(self):
@@ -35,6 +38,7 @@ class ResultsIterator:
         return self.results_list
     
     def save_results(self):
+        """ Save the results in a csv file """
         with open('results.csv','w') as f:
             f.write('id,url,search_terms,scrape_timestamp\n')
             for result in self.results_list:
