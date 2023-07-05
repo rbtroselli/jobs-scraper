f9412c33e3785eca5cc27565ccafe1c79cbff049
rbtroselli <rbt.roselli@gmail.com>
2023-07-05 22:43:08 +0200

    cleaner id

--- result_page.py
+++ result_page.py
@@ -15,7 +15,7 @@ class ResultPage:
         self.driver.get(self.url)
         raw_results = self.driver.find_elements('class name', 'jcs-JobTitle.css-jspxzf.eu4oa1w0')
         for raw_result in raw_results:
-            id = raw_result.get_attribute('id').replace('job_','')
+            id = raw_result.get_attribute('data-jk')
             self.results_list.append({
                 'id': id,
                 'url': f'https://www.indeed.com/viewjob?jk={id}',
@@ -26,10 +26,8 @@ class ResultPage:
     
     def display(self):
         for result in self.results_list:
-            print(result['id'])
-            print(result['url'])
-            print(result['search_terms'])
-            print(result['scrape_timestamp'])
+            for key, value in result.items():
+                print(f'{key}: {value}')
         return
 
     def get_results_dict_list(self):
