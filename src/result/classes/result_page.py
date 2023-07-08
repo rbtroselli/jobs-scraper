94313085981845b5a82d058a40e7aefadf8dad4b
rbtroselli <rbt.roselli@gmail.com>
2023-07-08 16:53:58 +0200

    skip result if search terms are not in title

--- src/result/classes/result_page.py
+++ src/result/classes/result_page.py
@@ -17,10 +17,14 @@ class ResultPage:
         raw_results = self.driver.find_elements('class name', 'jcs-JobTitle.css-jspxzf.eu4oa1w0')
         for raw_result in raw_results:
             id = raw_result.get_attribute('data-jk')
+            title = raw_result.text
+            if any(word.lower() not in title.lower() for word in self.search_terms.split(' ')):
+                continue # skip result if title does not contain all search terms
             self.results_list.append({
                 'id': id,
                 'url': f'https://www.indeed.com/viewjob?jk={id}',
                 'search_terms': self.search_terms,
+                'title': title,
                 'site_country': self.site_country,
                 'scrape_timestamp': self.scrape_timestamp
             })
