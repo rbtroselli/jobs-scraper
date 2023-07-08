47c525431e8267dd82318700a7459c1b77797ced
rbtroselli <rbt.roselli@gmail.com>
2023-07-08 16:02:57 +0200

    replace '' with None

--- src/post/classes/post.py
+++ src/post/classes/post.py
@@ -19,51 +19,41 @@ class Post:
         """ Scrape the job post """
         self.driver.get(self.post_dict['url'])
         # replace space in class names with dots
-        self.post_dict['content'] = self.driver.find_element('id','jobDescriptionText').text.replace('\n',' ')
+        self.post_dict['content'] = self.driver.find_element('id','jobDescriptionText').text #.replace('\n',' ').replace('\r', ' ')
         # dict.get(key,alternative) returns alternative if it cant find key. fail safe
         script_json = self.driver.find_element('xpath', "//script[@type = 'application/ld+json']").get_attribute('innerHTML')
         script_dict = json.loads(script_json)
-        self.post_dict['title'] = script_dict.get('title','')
-        self.post_dict['posted_timestamp'] = script_dict.get('datePosted','')
-        self.post_dict['address_country'] = script_dict.get('jobLocation','').get('address','').get('addressCountry','')
-        self.post_dict['address_locality'] = script_dict.get('jobLocation','').get('address','').get('addressLocality','')
-        self.post_dict['address_region_0'] = script_dict.get('jobLocation','').get('address','').get('addressRegion','')
-        self.post_dict['address_region_1'] = script_dict.get('jobLocation','').get('address','').get('addressRegion1','')
-        self.post_dict['address_region_2'] = script_dict.get('jobLocation','').get('address','').get('addressRegion2','')
-        self.post_dict['postal_code'] = script_dict.get('jobLocation','').get('address','').get('postalCode','')
-        self.post_dict['hiring_organization'] = script_dict.get('hiringOrganization','').get('name','')
+        self.post_dict['title'] = script_dict.get('title', None)
+        self.post_dict['posted_timestamp'] = script_dict.get('datePosted', None)
+        self.post_dict['address_country'] = script_dict.get('jobLocation', None).get('address', None).get('addressCountry', None)
+        self.post_dict['address_locality'] = script_dict.get('jobLocation', None).get('address', None).get('addressLocality', None)
+        self.post_dict['address_region_0'] = script_dict.get('jobLocation', None).get('address', None).get('addressRegion', None)
+        self.post_dict['address_region_1'] = script_dict.get('jobLocation', None).get('address', None).get('addressRegion1', None)
+        self.post_dict['address_region_2'] = script_dict.get('jobLocation', None).get('address', None).get('addressRegion2', None)
+        self.post_dict['postal_code'] = script_dict.get('jobLocation', None).get('address', None).get('postalCode', None)
+        self.post_dict['hiring_organization'] = script_dict.get('hiringOrganization', None).get('name', None)
         try:
-            self.post_dict['country_requirements'] = script_dict.get('applicantLocationRequirements','').get('name','')
+            self.post_dict['country_requirements'] = script_dict.get('applicantLocationRequirements', None).get('name', None)
         except:
             pass
         try:
-            self.post_dict['salary_currency'] = script_dict.get('baseSalary','').get('currency','')
-            self.post_dict['min_salary'] = script_dict.get('baseSalary','').get('value','').get('minValue','')
-            self.post_dict['max_salary'] = script_dict.get('baseSalary','').get('value','').get('maxValue','')
-            self.post_dict['salary'] = script_dict.get('baseSalary','').get('value','').get('value','')
-            self.post_dict['salary_unit'] = script_dict.get('baseSalary','').get('value','').get('unitText','')
+            self.post_dict['salary_currency'] = script_dict.get('baseSalary', None).get('currency', None)
+            self.post_dict['min_salary'] = script_dict.get('baseSalary', None).get('value', None).get('minValue', None)
+            self.post_dict['max_salary'] = script_dict.get('baseSalary', None).get('value', None).get('maxValue', None)
+            self.post_dict['salary'] = script_dict.get('baseSalary', None).get('value', None).get('value', None)
+            self.post_dict['salary_unit'] = script_dict.get('baseSalary', None).get('value', None).get('unitText', None)
         except:
             pass
-        self.post_dict['job_location_type'] = script_dict.get('jobLocationType','')
-        self.post_dict['employment_type'] = script_dict.get('employmentType','')
-        self.post_dict['valid_through_timestamp'] = script_dict.get('validThrough','')
-        self.post_dict['direct_apply'] = script_dict.get('directApply','')
+        self.post_dict['job_location_type'] = script_dict.get('jobLocationType', None)
+        self.post_dict['employment_type'] = script_dict.get('employmentType', None)
+        self.post_dict['valid_through_timestamp'] = script_dict.get('validThrough', None)
+        self.post_dict['direct_apply'] = script_dict.get('directApply', None)
         # self.post_dict['raw_script_json'] = str(script_json)
-        return
-    
-    def _clean_separator_and_empty(self):
-        """ Iterate through the post_dict, remove any empty strings and replace | with - """
-        for key, value in self.post_dict.items():
-            if value == '':
-                self.post_dict[key] = None
-            if type(value)==str and '|' in value:
-                self.post_dict[key] = value.replace('|','-')
         return
     
     def _assign_post_elements(self):
         try:
             self._scrape()
-            self._clean_separator_and_empty()
         except:
             pass
         return
